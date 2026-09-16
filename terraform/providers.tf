a8b88c37a0c35bb4eeffd370ae400d5d1142c35b
lzdravecky <163843980+lzdravecky@users.noreply.github.com>
2026-09-16 13:24:55 +0200

    Configure S3 backend for Terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 6.64"
     }
   }
+
+  backend "s3" {
+    bucket       = "gymnathlon-terraform-state-15963"
+    key          = "gymnathlon-checker/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
